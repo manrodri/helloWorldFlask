c10694511c3abe389cb612c7dc6fb260ae1b3548
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2019-08-28 07:42:10 +0100

    end part III megatutorial

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,32 @@
 from app import app
+from flask import render_template, flash, redirect, url_for
+from app.forms import LogingForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {
+        'username': "Manuel"
+    }
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LogingForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for("index"))
+    return render_template("login.html", form=form, title="Sign In")
